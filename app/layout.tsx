5cf45b4e46452bafafce5b6b2d44d78fc5d34da5
Minh Tu Ngo <loducfc@gmail.com>
2023-02-09 22:56:25 -0500

    Implement NextAuth with Firebase

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,34 @@
 import SideBar from '@/components/SideBar';
 import './globals.css';
+import { getServerSession } from 'next-auth';
 
-export default function RootLayout({
+import { authOptions } from '@/pages/api/auth/[...auth]';
+import Login from '@/components/Login';
+import SessionProvider from '@/components/SessionProvider';
+
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
+
   return (
     <html lang='en'>
       <head />
       <body className='flex'>
-        <div className='bg-[#0d0d0d] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]'>
-          <SideBar />
-        </div>
-        <div className='flex-1 bg-[#1a1a1a]'>{children}</div>
+        <SessionProvider session={session}>
+          {!session ? (
+            <Login />
+          ) : (
+            <>
+              <div className='bg-[#0d0d0d] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]'>
+                <SideBar />
+              </div>
+              <div className='flex-1 bg-[#1a1a1a]'>{children}</div>
+            </>
+          )}
+        </SessionProvider>
       </body>
     </html>
   );
